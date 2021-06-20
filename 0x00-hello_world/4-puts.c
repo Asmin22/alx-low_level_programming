546fabde355a67846561f60cbec7502e2b6a2d8c
Asmin22 <kizaasmin@gmail.com>
2021-06-20 10:51:33 -0700

    This is my first C program that prints

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - entry for  c coding challenge
+* in low level coding section.
+* Return: 0 returns zero
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
