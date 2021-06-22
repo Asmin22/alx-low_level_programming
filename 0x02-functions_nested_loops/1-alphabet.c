551d0b93b817342f80aa96f2c86b020dac63d53c
Asmin22 <kizaasmin@gmail.com>
2021-06-22 12:17:21 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+* print_alphabet - prints alphabets in lower case
+* Return: 0 - doesn't return anything
+*/
+void print_alphabet(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
+}
