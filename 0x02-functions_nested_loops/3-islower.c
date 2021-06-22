72b8738aed46f18d456cd10f490fed7f30423e34
Asmin22 <kizaasmin@gmail.com>
2021-06-22 12:38:11 -0700

    3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _islower - checks whether input value is lower of not
+ * @c: ascii value of char
+ * Return: 1 when lowercase , 0 when not.
+ */
+int _islower(int c)
+{
+  if (c > 96 && c < 123)
+    {
+      return (1);
+    }
+  else
+    {
+      return (0);
+    }
+}
