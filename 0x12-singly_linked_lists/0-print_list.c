190694e5294d061b1af184a69cf4b02e36275f64
Asmin22 <kizaasmin@gmail.com>
2021-08-04 10:09:29 -0700

    print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+* print_list - Entry Point
+*
+* @h: list argument
+*
+* Description: Prints all the elements of list_t
+*
+* Return: Number of nodes
+*/
+size_t print_list(const list_t *h)
+{
+size_t nod_idx;
+for (nod_idx = 0; h != NULL; nod_idx++)
+{
+if (h->str == NULL)
+{
+printf("[0] (nil)\n");
+}
+else
+{
+printf("[%d] %s\n", h->len, h->str);
+}
+h = h->next;
+}
+return (nod_idx);
+}
